6905f2e32d623a89370abf18a6bd3328c53a4a2e
sariya <dayzwaitik123@yandex.ru>
2024-03-28 21:11:32 +0300

    Add property of size to board

--- board/board.go
+++ board/board.go
@@ -7,26 +7,47 @@ import (
 
 type Board struct {
 	Board [][]string
+	SizeX int
+	SizeY int
 }
 
 func (b *Board) NewBoard() Board {
 	for i := 0; i < 3; i++ {
 		b.Board = append(b.Board, []string{"*", "*", "*"})
 	}
-
+	b.SizeX = 3
+	b.SizeY = 3
 	return *b
 }
 
 func (b *Board) DrawBoard() {
 	fmt.Println(strings.Repeat("-", 15))
 
-	for i := 0; i < len(b.Board); i++ {
+	for i := 0; i < b.SizeX; i++ {
 		row := ""
-		for j := 0; j < len(b.Board[i]); j++ {
+		for j := 0; j < b.SizeY; j++ {
 			row += fmt.Sprintf("| %s |", b.Board[i][j])
 		}
 		fmt.Println(row)
 		fmt.Println(strings.Repeat("-", 15))
 	}
 
+}
+
+func (b *Board) IsGameEnd() bool {
+	var countEmptyFields int
+
+	for i := 0; i < b.SizeX; i++ {
+		for j := 0; j < b.SizeY; j++ {
+			if b.Board[i][j] == "*" {
+				countEmptyFields++
+			}
+		}
+	}
+
+	if countEmptyFields > 0 {
+		return false
+	}
+
+	return true
 }
