e8411d824f8bd8568c4f6f951f314d396d7084bb
sariya <dayzwaitik123@yandex.ru>
2024-03-28 21:11:03 +0300

    Add test

---
+++ board/board_test.go
@@ -0,0 +1,29 @@
+package board
+
+import "testing"
+
+type testBoardPairs struct {
+	board     Board
+	isGameEnd bool
+}
+
+var tests = []testBoardPairs{
+	{board: Board{Board: [][]string{{"X", "X", "X"}, {"O", "X", "O"}, {"X", "O", "X"}}, SizeX: 3, SizeY: 3}, isGameEnd: true},
+	{board: Board{Board: [][]string{{"*", "X", "X"}, {"O", "X", "O"}, {"X", "O", "X"}}, SizeX: 3, SizeY: 3}, isGameEnd: false},
+	{board: Board{Board: [][]string{{"*", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}}, SizeX: 3, SizeY: 3}, isGameEnd: false},
+	{board: Board{Board: [][]string{{"X", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}}, SizeX: 3, SizeY: 3}, isGameEnd: false},
+}
+
+func TestIsGameEnd(t *testing.T) {
+	for _, pair := range tests {
+		v := pair.board.IsGameEnd()
+
+		if v != pair.isGameEnd {
+			t.Error(
+				"For", pair.board,
+				"expected", pair.isGameEnd,
+				"got", v,
+			)
+		}
+	}
+}
