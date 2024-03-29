da5c4fcaac8e042f14a5e3d7a8057a82ea49927c
sariya <dayzwaitik123@yandex.ru>
2024-03-29 10:17:56 +0300

    test for new board

--- board/board_test.go
+++ board/board_test.go
@@ -1,10 +1,19 @@
 package board
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 type testStructIsGameEnd struct {
 	board             Board
 	expectedIsGameEnd bool
+}
+
+var expectedNewBoard [][]string = [][]string{
+	{"*", "*", "*"},
+	{"*", "*", "*"},
+	{"*", "*", "*"},
 }
 
 var testsIsGameEnd = []testStructIsGameEnd{
@@ -12,6 +21,16 @@ var testsIsGameEnd = []testStructIsGameEnd{
 	{board: Board{Board: [][]string{{"*", "X", "X"}, {"O", "X", "O"}, {"X", "O", "X"}}, SizeX: 3, SizeY: 3}, expectedIsGameEnd: false},
 	{board: Board{Board: [][]string{{"*", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}}, SizeX: 3, SizeY: 3}, expectedIsGameEnd: false},
 	{board: Board{Board: [][]string{{"X", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}}, SizeX: 3, SizeY: 3}, expectedIsGameEnd: false},
+}
+
+func TestNewBoard(t *testing.T) {
+	var b Board
+	b = b.NewBoard()
+	actualBoard := b.Board
+
+	if !reflect.DeepEqual(actualBoard, expectedNewBoard) {
+		t.Error("New board created wrong")
+	}
 }
 
 func TestIsGameEnd(t *testing.T) {
