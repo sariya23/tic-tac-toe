273874e78f367a5e301f7cdc0d156d2511c7a3c5
sariya <dayzwaitik123@yandex.ru>
2024-03-29 10:01:21 +0300

    Rename test va

--- board/board_test.go
+++ board/board_test.go
@@ -7,7 +7,7 @@ type testBoardPairs struct {
 	isGameEnd bool
 }
 
-var tests = []testBoardPairs{
+var testsIsGameEnd = []testBoardPairs{
 	{board: Board{Board: [][]string{{"X", "X", "X"}, {"O", "X", "O"}, {"X", "O", "X"}}, SizeX: 3, SizeY: 3}, isGameEnd: true},
 	{board: Board{Board: [][]string{{"*", "X", "X"}, {"O", "X", "O"}, {"X", "O", "X"}}, SizeX: 3, SizeY: 3}, isGameEnd: false},
 	{board: Board{Board: [][]string{{"*", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}}, SizeX: 3, SizeY: 3}, isGameEnd: false},
@@ -15,7 +15,7 @@ var tests = []testBoardPairs{
 }
 
 func TestIsGameEnd(t *testing.T) {
-	for _, pair := range tests {
+	for _, pair := range testsIsGameEnd {
 		v := pair.board.IsGameEnd()
 
 		if v != pair.isGameEnd {
