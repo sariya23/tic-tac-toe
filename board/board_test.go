fd871fe36388aee92ca4570536829da0c1d0d5e0
sariya <dayzwaitik123@yandex.ru>
2024-03-29 11:06:42 +0300

    Test for availableSteps

--- board/board_test.go
+++ board/board_test.go
@@ -10,7 +10,12 @@ type testStructIsGameEnd struct {
 	expectedIsGameEnd bool
 }
 
-var expectedNewBoard [][]string = [][]string{
+type testStructGetAvailableSteps struct {
+	board         Board
+	expectedSteps []Step
+}
+
+var expectedNewBoard = [][]string{
 	{"*", "*", "*"},
 	{"*", "*", "*"},
 	{"*", "*", "*"},
@@ -21,6 +26,50 @@ var testsIsGameEnd = []testStructIsGameEnd{
 	{board: Board{Board: [][]string{{"*", "X", "X"}, {"O", "X", "O"}, {"X", "O", "X"}}, SizeX: 3, SizeY: 3}, expectedIsGameEnd: false},
 	{board: Board{Board: [][]string{{"*", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}}, SizeX: 3, SizeY: 3}, expectedIsGameEnd: false},
 	{board: Board{Board: [][]string{{"X", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}}, SizeX: 3, SizeY: 3}, expectedIsGameEnd: false},
+}
+
+var testsGetAvailableSteps = []testStructGetAvailableSteps{
+	{
+		board: Board{
+			Board: [][]string{{"*", "*", "*"}, {"*", "*", "*"}, {"*", "*", "*"}},
+			SizeX: 3,
+			SizeY: 3,
+		},
+		expectedSteps: []Step{
+			{0, 0},
+			{0, 1},
+			{0, 2},
+			{1, 0},
+			{1, 1},
+			{1, 2},
+			{2, 0},
+			{2, 1},
+			{2, 2},
+		},
+	},
+	{
+		board: Board{
+			Board: [][]string{{"X", "X", "X"}, {"O", "X", "O"}, {"X", "O", "X"}},
+			SizeX: 3,
+			SizeY: 3,
+		},
+		expectedSteps: []Step{},
+	},
+	{
+		board: Board{
+			Board: [][]string{
+				{"X", "X", "X"},
+				{"O", "*", "O"},
+				{"X", "O", "*"},
+			},
+			SizeX: 3,
+			SizeY: 3,
+		},
+		expectedSteps: []Step{
+			{1, 1},
+			{2, 2},
+		},
+	},
 }
 
 func TestNewBoard(t *testing.T) {
@@ -41,6 +90,19 @@ func TestIsGameEnd(t *testing.T) {
 			t.Error(
 				"For", pair.board,
 				"expected", pair.expectedIsGameEnd,
+				"got", actual,
+			)
+		}
+	}
+}
+
+func TestGetAvailableSteps(t *testing.T) {
+	for _, pair := range testsGetAvailableSteps {
+		actual := pair.board.GetAvailableSteps()
+		if !reflect.DeepEqual(actual, pair.expectedSteps) {
+			t.Error(
+				"For", pair.board,
+				"expected", pair.expectedSteps,
 				"got", actual,
 			)
 		}
